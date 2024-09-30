a33f851a8f5ebfcc27a897692ed2da0555dbd08a
yangyijie816 <zhc123001@163.com>
2024-09-30 18:38:41 +0800

    详情页

---
+++ A2-clientside/js/index.js
@@ -0,0 +1,17 @@
+function search() {
+  console.log('search')
+}
+
+function clearCheckboxes() {
+  console.log('clearCheckboxes')
+}
+
+function toDetails(path) {
+  window.location.href = './details.html'
+}
+
+function template(data) {
+  return `
+		<div>search</div>
+	`
+}
