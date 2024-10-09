a03495e8208b342e35cd95cb86220c933ffd6346
yangyijie816 <zhc123001@163.com>
2024-10-09 20:52:39 +0800

    修改端口号

--- A2-api/crowdfunding_db.js
+++ A2-api/crowdfunding_db.js
@@ -3,7 +3,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 // Default port number
-const port = 3000
+const port = 3090
 
 app.use(express.json())
 // Cross-domain prevention
