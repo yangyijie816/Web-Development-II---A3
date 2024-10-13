a4e8cf9a91bfad81f01195e27e8675eb24264704
yangyijie816 <zhc123001@163.com>
2024-10-14 04:02:00 +0800

    Create a search route

--- ng-clientside/src/app/app.routes.ts
+++ ng-clientside/src/app/app.routes.ts
@@ -1,4 +1,8 @@
 import { Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component'
+import { CrowdfundingComponent } from './crowdfunding/crowdfunding.component'
 
-export const routes: Routes = [{ path: '', component: HomeComponent, title: 'home' }]
+export const routes: Routes = [
+  { path: '', component: HomeComponent, title: 'home' },
+  { path: 'search', component: CrowdfundingComponent, title: 'Crowdfunding' },
+]
