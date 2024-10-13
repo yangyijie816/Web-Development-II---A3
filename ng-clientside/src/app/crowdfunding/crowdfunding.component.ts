44edb88e13dd54aa7968d42133f0887d5f8bcd14
yangyijie816 <zhc123001@163.com>
2024-10-14 03:59:48 +0800

    Delete useless git configuration files on the client

---
+++ ng-clientside/src/app/crowdfunding/crowdfunding.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crowdfunding',
+  standalone: true,
+  imports: [],
+  templateUrl: './crowdfunding.component.html',
+  styleUrl: './crowdfunding.component.css'
+})
+export class CrowdfundingComponent {
+
+}
