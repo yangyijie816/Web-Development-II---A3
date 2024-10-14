95eceb5f1612d948a8b2b168eb684cdd107a4468
yangyijie816 <zhc123001@163.com>
2024-10-14 08:39:06 +0800

    Remove native development projects using html

--- A3-clientside/service.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const path = require('path')
-const app = express()
-app.use(express.static(path.join(__dirname)))
-const PORT = 8080
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
